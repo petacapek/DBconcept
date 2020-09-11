1b162c04b483bf12d13e6fe2cf0fc5704b3847d2
Petr Čapek <petacapek@gmail.com>
2020-09-11 12:47:24 +0200

    Marstorp and Witter, 1999 paper - analysis done

--- model_runs.R
+++ model_runs.R
@@ -35,7 +35,7 @@ theme_min<-theme(axis.text.x=element_text(vjust=0.2, size=18, colour="black"),
 ############################################################################################################################
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #DATA
-mar<-read.csv("../Marstorp1999.csv", sep=',')
+mar<-read.csv("Data/Marstorp1999.csv", sep=',')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Sub-microbial model~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Model definition
@@ -66,7 +66,7 @@ SUBmodel<-function(time, state, pars){
   }
   
 #Goodness of fit
-good<-function(x){
+good_sub<-function(x){
     p<-x
     names(p)<-c("v", "k", "f", "m", "Y", "fr", "fd")
     #Initial Br and Bs
@@ -74,14 +74,14 @@ good<-function(x){
     #Br_i<-(mar$Cmicinit[1]-p[["fs"]]*Bs_i)/p[["fr"]]
     #Simulations
     yhat_all<-as.data.frame(ode(y=c(G=mar$Sinit[1], Br=0, Bs=Bs_i, CO2=0),
-                                func = Twopool, parms=p,
+                                func = SUBmodel, parms=p,
                                 times = as.numeric(mar$Time)))
     #Selecting measured variables
-    yhat<-yhat_all[, c("time", "CO2", "DNA", "CFC", "G")]
+    yhat<-yhat_all[, c("time", "CO2", "DNA", "CFC14", "CFC", "G")]
     #Long format
     Yhat<-melt(yhat, id.vars=c("time"))
     #Observations
-    Yhat$obs<-c(as.numeric(mar$CO212cumul), as.numeric(mar$DNA), #, as.numeric(mar$Cmic14)
+    Yhat$obs<-c(as.numeric(mar$CO212cumul), as.numeric(mar$DNA), as.numeric(mar$Cmic14),
                 as.numeric(mar$Cmic12+mar$Cmic14), as.numeric(mar$S))
     Gfit<-Yhat %>% group_by(variable) %>% summarise(SSres=sum(((obs-value)^2), na.rm = T),
                                                     SStot=sum(((obs-mean(obs, na.rm = T))^2), na.rm = T),
@@ -92,7 +92,7 @@ good<-function(x){
     
     #Fine temporal scale fo r graphs
     yhat_all_fine<-as.data.frame(ode(y=c(G=mar$Sinit[1], Br=0, Bs=Bs_i, CO2=0),
-                                     func = Twopool, parms=p,
+                                     func = SUBmodel, parms=p,
                                      times = seq(0, 8.5, by=0.1)))
     Yhat_all_fine<-melt(yhat_all_fine, id.vars=c("time"))
     
@@ -102,18 +102,29 @@ good<-function(x){
   }
   
 #Read parameters estimated in python
-opt_par<-as.numeric(read.csv("/home/capekp00/opt_parsa.csv", header = F))
+marstorp_optpar<-as.numeric(read.csv("parameters/marstorp_optpars.csv", header = F))
     
-Twopool_fit<-good(opt_par)
-as.data.frame(Twopool_fit$Gfit)
-    
+Marstorp_fit<-good_sub(marstorp_optpar)
+as.data.frame(Marstorp_fit$Gfit)
+
 #Figure
-ggplot(Twopool_fit$Yhat, aes(time, obs))+geom_point(cex=6, pch=21, fill="grey")+
-    geom_line(data=Twopool_fit$Yhat_fine, aes(time, value))+
-    facet_wrap(~variable, scales="free")
+Marstorp_fit$Yhat$variable2<-Marstorp_fit$Yhat$variable
+levels(Marstorp_fit$Yhat$variable2)<-c("CO[2]", "DNA", "MB^{14}~C", "MBC", "Glucose")
 
+Marstorp_fit$Yhat_fine$variable2<-Marstorp_fit$Yhat_fine$variable
+levels(Marstorp_fit$Yhat_fine$variable2)<-c("Glucose", "Br", "Bs", "CO[2]", "MBC", "MB^{14}~C", "DNA", "Respiration")
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Monod model~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+ggplot(subset(Marstorp_fit$Yhat, variable=="CO2" | variable=="DNA" | variable=="CFC14" | 
+                variable=="CFC" | variable=="G"), aes(time, obs))+
+  geom_point(cex=6, pch=21, fill="grey")+
+  geom_line(data=subset(Marstorp_fit$Yhat_fine, variable=="CO2" | variable=="DNA" | variable=="CFC14" | 
+                          variable=="CFC" | variable=="G"), aes(time, value), lwd=1.2, color="grey30")+theme_min+
+  facet_wrap(~variable2, scales="free", labeller = label_parsed) + 
+  ylab(expression(paste("Carbon pool (", mu, "mol ", g(DW)^{-1}, ")"))) +
+  xlab("Time (days)")
+
+    
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Monod model~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Model definition
 Monod<-function(time, state, pars){
   with(as.list(c(state, pars)),{
@@ -132,28 +143,29 @@ Monod<-function(time, state, pars){
     #States
     dB<- uptake*Y - decay
     dG<- - uptake
+    dCO2<- uptake*(1-Y)
     
-    return(list(c(dB, dG), CFC=CFC, CFC14=CFC14, DNA=DNA, r=r))
+    return(list(c(dB, dG, dCO2), CFC=CFC, CFC14=CFC14, DNA=DNA, r=r))
   })
 }
 
 #Goodness of fit
-good<-function(x){
+monodgood<-function(x){
   p<-x
   names(p)<-c("v", "k", "m", "Y", "kec", "kd")
   #Initial Br and Bs
-  B_i<-mar$Cmicinit[1]/p[["kec"]]
+  B_i<-mar$DNAinit[1]/p[["kd"]]
   
   #Simulations
-  yhat_all<-as.data.frame(ode(y=c(B=B_i, G=mar$Sinit[1]),
+  yhat_all<-as.data.frame(ode(y=c(B=B_i, G=mar$Sinit[1], CO2=0),
                               func = Monod, parms=p,
                               times = as.numeric(mar$Time)))
   #Selecting measured variables
-  yhat<-yhat_all[, c("time", "r", "DNA", "CFC", "CFC14", "G")]
+  yhat<-yhat_all[, c("time", "CO2", "DNA", "CFC", "CFC14", "G")]
   #Long format
   Yhat<-melt(yhat, id.vars=c("time"))
   #Observations
-  Yhat$obs<-c(as.numeric(mar$CO212), as.numeric(mar$DNA), as.numeric(mar$Cmic12+mar$Cmic14),
+  Yhat$obs<-c(as.numeric(mar$CO212cumul), as.numeric(mar$DNA), as.numeric(mar$Cmic12+mar$Cmic14),
                as.numeric(mar$Cmic14), as.numeric(mar$S))
   Gfit<-Yhat %>% group_by(variable) %>% summarise(SSres=sum(((obs-value)^2), na.rm = T),
                                                   SStot=sum(((obs-mean(obs, na.rm = T))^2), na.rm = T),
@@ -163,7 +175,7 @@ good<-function(x){
   Gfit$AIC<-with(Gfit, 2*N-2*ll)
   
   #Fine temporal scale for graphs
-  yhat_all_fine<-as.data.frame(ode(y=c(B=B_i, G=mar$Sinit[1]),
+  yhat_all_fine<-as.data.frame(ode(y=c(B=B_i, G=mar$Sinit[1], CO2=0),
                                    func = Monod, parms=p,
                                    times = seq(0, 8.5, by=0.1)))
   Yhat_all_fine<-melt(yhat_all_fine, id.vars=c("time"))
@@ -174,39 +186,55 @@ good<-function(x){
 }
 
 #Read parameters estimated in python
-Monod_opt<-as.numeric(read.csv("../monod_opt.csv", header = F))
+marstorp_monodopt<-as.numeric(read.csv("parameters/marstorp_monodpars.csv", header = F))
 
-Monod_fit<-good(Monod_opt)
-as.data.frame(Monod_fit$Gfit)
-
-as.data.frame(Monod_fit$Gfit$R2)
-as.data.frame(Twopool_fit$Gfit$R2)
+Marstorp_monodfit<-monodgood(marstorp_monodopt)
+as.data.frame(Marstorp_monodfit$Gfit)
 
 #Figure
-ggplot(Monod_fit$Yhat, aes(time, obs))+geom_point(cex=6, pch=21, fill="grey")+
-  geom_line(data=Monod_fit$Yhat_fine[, c("time", "variable", "value")], 
-            aes(time, value))+
-  facet_wrap(~variable, scales="free")+
-  geom_line(data=Twopool_fit$Yhat_fine[, c("time", "variable", "value")], 
-            aes(time, value), color="blue")
+Marstorp_monodfit$Yhat_fine$variable2<-Marstorp_monodfit$Yhat_fine$variable
+levels(Marstorp_monodfit$Yhat_fine$variable2)<-c("B", "Glucose",  "CO[2]", "MBC", "MB^{14}~C", "DNA", "Respiration")
+
+Marstorp_fita<-subset(Marstorp_fit$Yhat_fine, variable=="CO2" | variable=="DNA" | variable=="CFC14" | 
+                        variable=="CFC" | variable=="G")
+Marstorp_fita$Model<-c("Sub-microbial")
+Marstorp_fitb<-subset(Marstorp_monodfit$Yhat_fine, variable=="CO2" | variable=="DNA" | variable=="CFC14" | 
+                        variable=="CFC" | variable=="G")
+Marstorp_fitb$Model<-c("Monod")
+
+Marstorp_fit_all<-rbind(Marstorp_fita, Marstorp_fitb)
+
+ggplot(subset(Marstorp_fit$Yhat, variable=="CO2" | variable=="DNA" | variable=="CFC14" | 
+                variable=="CFC" | variable=="G"), aes(time, obs))+
+  geom_point(cex=6, pch=21, fill="grey")+
+  geom_line(data=Marstorp_fit_all, aes(time, value, color=Model), lwd=1.2)+theme_min+
+  facet_wrap(~variable2, scales="free", labeller = label_parsed) + 
+  ylab(expression(paste("Carbon pool (", mu, "mol ", g(DW)^{-1}, ")"))) +
+  xlab("Time (days)") + scale_color_manual(values = c("indianred", "grey30")) +
+  theme(legend.position = c(0.85, 0.1))
         
-######################################Statistics####################################
-#F test - based on residual sum of squares, number of parameters 
-#and number of measurements
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Statistics~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#Log Likelihood ratio test
+-2*(Marstorp_monodfit$Gfit$ll-Marstorp_fit$Gfit$ll)
+
+round(pchisq(-2*(Marstorp_monodfit$Gfit$ll-Marstorp_fit$Gfit$ll), df=(length(marstorp_optpar)-length(marstorp_monodopt)),
+       lower.tail = F), 3)
+
+#F test - based on residual sum of squares, number of parameters and number of measurements
 ##Monod model
 ###residual sum of squares
-M1ss = sum((Monod_fit$Yhat$obs-Monod_fit$Yhat$value)^2, na.rm = T)
+M1ss = sum((Marstorp_monodfit$Yhat$obs-Marstorp_monodfit$Yhat$value)^2, na.rm = T)
 ###number of parameters 
-M1p = length(Monod_opt)
+M1p = length(marstorp_monodopt)
 
-##Two pool model
+##Sub-microbial model
 ###residual sum of squares
-M2ss = sum((Twopool_fit$Yhat$obs-Twopool_fit$Yhat$value)^2, na.rm = T)
+M2ss = sum((Marstorp_fit$Yhat$obs-Marstorp_fit$Yhat$value)^2, na.rm = T)
 ###number of parameters 
-M2p = length(opt_par)
+M2p = length(marstorp_optpar)
 
 ###total number of measurements
-nt = nrow(Twopool_fit$Yhat[!is.na(Twopool_fit$Yhat), ])
+nt = nrow(Marstorp_fit$Yhat[!is.na(Marstorp_fit$Yhat), ])
 
 ####F value =  (M1ss - M2ss)*(nt - M2p)/M2ss/(M2p - M1p)
 (M1ss - M2ss)*(nt - M2p)/M2ss/(M2p - M1p)
